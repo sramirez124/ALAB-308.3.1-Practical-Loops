4dd9f911b95c5f79477c0eafe4c89cf24b4ccc6f
sramirez124 <forwardemails124@gmail.com>
2024-09-16 10:56:04 -0500

    part 3 continue
    following along in class with the code

--- practical-loops.js
+++ practical-loops.js
@@ -1,5 +1,5 @@
 // Part 1: Fizz Buzz
-
+console.log("==================== Part 1 ====================");
 for (let x = 0; x <= 100; x++) {
     if (x % 3 === 0 && x % 5 === 0){
        // console.log("Fizz Buzz")
@@ -13,7 +13,7 @@ for (let x = 0; x <= 100; x++) {
 }
 
 // Part 2: Prime Time
-
+console.log("==================== Part 2 ====================");
 let n = 1171;
 let nextPrime;
 let i = n + 1;
@@ -29,4 +29,38 @@ outerLoop: while (!nextPrime) {
 
     nextPrime = i;
 }
-console.log("Next prime number after ", n , " is " , nextPrime);
+// console.log("Next prime number after ", n , " is " , nextPrime);
+
+// Part 3: Feeling Loopy
+const csvString = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
+
+let cell1 = '';
+let cell2 = '';
+let cell3 = '';
+let cell4 = '';
+
+let currentCell = 1;
+
+for (let i = 0; i < csvString.length; i++) {
+
+    if (csvString[i] === ","){
+        currentCell++;
+        continue;
+    }
+
+    switch (currentCell){
+        case 1:
+            cell1 += csvString[i];
+        break;
+        case 2:
+            cell2 += csvString[i];
+        break;
+        case 3:
+            cell3 += csvString[i];
+        break;
+        case 4:
+            cell4 += csvString[i];
+        break;
+    }
+}
+console.log(cell1, cell2, cell3, cell4);
