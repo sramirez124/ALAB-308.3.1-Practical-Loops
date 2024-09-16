7584828262af750d737243acc78d3e9af56973ab
sramirez124 <forwardemails124@gmail.com>
2024-09-16 10:19:52 -0500

    Part 1 done

--- practical-loops.js
+++ practical-loops.js
@@ -1,9 +1,13 @@
 // Part 1: Fizz Buzz
 
 for (let x = 0; x <= 100; x++) {
-    if (){
-
-    } else if (){
-
-    } else
+    if (x % 3 === 0 && x % 5 === 0){
+        console.log("Fizz Buzz")
+    } else if (x % 3 === 0){
+        console.log("Fizz")
+    } else if (x % 5 === 0){
+        console.log("Buzz")
+    } else {
+        console.log(x);
+    }
 }
