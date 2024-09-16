4ab7dd12b36cc3cca3a8cbe445583abc2d44e5cd
sramirez124 <forwardemails124@gmail.com>
2024-09-16 10:40:21 -0500

    part 2 works
    had an error because I filled out the while loop wrong and took some debugging to find it

--- practical-loops.js
+++ practical-loops.js
@@ -2,16 +2,31 @@
 
 for (let x = 0; x <= 100; x++) {
     if (x % 3 === 0 && x % 5 === 0){
-        console.log("Fizz Buzz")
+       // console.log("Fizz Buzz")
     } else if (x % 3 === 0){
-        console.log("Fizz")
+       // console.log("Fizz")
     } else if (x % 5 === 0){
-        console.log("Buzz")
+       // console.log("Buzz")
     } else {
-        console.log(x);
+       // console.log(x);
     }
 }
 
 // Part 2: Prime Time
 
-let n;
+let n = 1171;
+let nextPrime;
+let i = n + 1;
+
+// !nextPrime is short hand for nextPrime === undefined
+outerLoop: while (!nextPrime) {
+    for (let j = 2; j < i; j++) {
+        if (i % j === 0){
+            i++
+            continue outerLoop;
+        }
+    }
+
+    nextPrime = i;
+}
+console.log("Next prime number after ", n , " is " , nextPrime);
